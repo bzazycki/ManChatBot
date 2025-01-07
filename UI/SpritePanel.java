64e891b0f08d956d5ae304760d45b6d507fd98df
Lillian Elliott <lilliankiana22@gmail.com>
2025-01-07 15:39:50 +0000

    Sprite Animation added, no errors but also no display

--- UI/SpritePanel.java
+++ UI/SpritePanel.java
@@ -4,12 +4,15 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Timer;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
+import javax.swing.Timer;
 
 
 
@@ -24,29 +27,8 @@ public class SpritePanel extends JPanel {
     /**
      * JPanel that contains the sprite animations.
      */
-    private JPanel spritePanel;
-
-    ImageIcon gifIcon = new ImageIcon("UI\\Sprite.gif");
-
-    JLabel gifLabel = new JLabel(gifIcon);
-    gifLabel.setVerticalAlignment(SwingConstants.CENTER);
-    add(gifLabel);
-
-    timer = new Timer(1000, new ActionListener()) {
-      @Override
-      public void actionPerformed(ActionEvent e) {
-        if (isPlaying) {
-          elapsedTime++;
-          if (elapsedTime >= 30) {
-              stopGif();
-          }
-          // Simulate the progress of the GIF (you could add more actions here)
-        }
-      }
-    });
     
-    timer.start();
-
+    private JPanel spritePanel;
     // === *** Constructors *** //
 
     /**
@@ -54,9 +36,44 @@ public class SpritePanel extends JPanel {
      * to.
      */
     public SpritePanel() {
-        super(new BorderLayout());
+      super(new BorderLayout());
 
-        this.add(spritePanel());
+      ImageIcon gifIcon = new ImageIcon("UI\\Sprite.gif");
+
+      JLabel gifLabel = new JLabel(gifIcon);
+      gifLabel.setVerticalAlignment(SwingConstants.CENTER);
+      add(gifLabel);
+
+      timer = new Timer(1000, new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+          if (isPlaying) {
+            elapsedTime++;
+            if (elapsedTime >= 30) {
+              stopGif();
+            }
+          // Simulate the progress of the GIF (you could add more actions here)
+          }
+        }
+      });
+    
+      timer.start();
+
+      this.add(spritePanel());
+    }
+
+    // Simulate starting the GIF (starts the timer)
+    private void startGif() {
+        isPlaying = true;
+        elapsedTime = 0;
+        timer.start();
+    }
+
+    // Stop the GIF after 30 seconds
+    private void stopGif() {
+        isPlaying = false;
+        timer.stop();
+        JOptionPane.showMessageDialog(this, "GIF Stopped After 30 Seconds");
     }
 
         /**
@@ -68,7 +85,16 @@ public class SpritePanel extends JPanel {
       JPanel spritePanel = new JPanel();
       spritePanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
       this.spritePanel = spritePanel;
+      
+      SwingUtilities.invokeLater(new Runnable() {
+        @Override
+        public void run() {
+          SpritePanel app = new SpritePanel();
+          app.setVisible(true);
+          app.startGif();  // Automatically start the GIF when the app launches
+        }
+      });
       return spritePanel;
-  }
+    }
   
 }
