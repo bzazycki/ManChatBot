e9a4ac94fcb93231d1c0a8e5f9a80952c899c133
Lillian Elliott <lilliankiana22@gmail.com>
2025-01-07 15:27:07 +0000

    Sprite Changes - Errors present

--- UI/SpritePanel.java
+++ UI/SpritePanel.java
@@ -2,12 +2,22 @@ package UI;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Timer;
 
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 
+
 public class SpritePanel extends JPanel {
+
+  private Timer timer;
+  private int elapsedTime = 0;
+  private boolean isPlaying = true;
 
   // === *** Attributes *** === //
 
@@ -16,10 +26,31 @@ public class SpritePanel extends JPanel {
      */
     private JPanel spritePanel;
 
+    ImageIcon gifIcon = new ImageIcon("UI\\Sprite.gif");
+
+    JLabel gifLabel = new JLabel(gifIcon);
+    gifLabel.setVerticalAlignment(SwingConstants.CENTER);
+    add(gifLabel);
+
+    timer = new Timer(1000, new ActionListener()) {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        if (isPlaying) {
+          elapsedTime++;
+          if (elapsedTime >= 30) {
+              stopGif();
+          }
+          // Simulate the progress of the GIF (you could add more actions here)
+        }
+      }
+    });
+    
+    timer.start();
+
     // === *** Constructors *** //
 
     /**
-     * Creates the Chat Panel. This panel will contain a schat box that can be added
+     * Creates the Sprite Panel. This panel will contain a schat box that can be added
      * to.
      */
     public SpritePanel() {
@@ -29,9 +60,9 @@ public class SpritePanel extends JPanel {
     }
 
         /**
-     * Creates the chat panel. This panel will be manipulated to
-     * show everything that was said and responded.
-     * @return the chat panel
+     * Creates the sprite panel. This panel will be manipulated to
+     * show the animations.
+     * @return the sprite panel
      */
     private JPanel spritePanel() {
       JPanel spritePanel = new JPanel();
